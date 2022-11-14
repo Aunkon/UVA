255c6398bba9373cf55c6ff8fa4b358fcf81d456
Md. Walid Bin Khalid Aunkon <mdwalidbinkhalidaunkon@gmail.com>
2022-11-14 23:00:18 +0600

    119 ttl 0.000 rank 4900

--- 119_Greedy_Gift_Givers.cpp
+++ 119_Greedy_Gift_Givers.cpp
@@ -2,50 +2,89 @@
 /*#####       Sr. Software Engineer (Unity)         #####*/
 /*#####       Email:  waunkon@gmail.com             #####*/
 /*#####       Mobile No:  +88-01916-492926          #####*/
-#include<stdio.h>
-
+// #include <iostream>
+// using namespace std;
+#include <stdio.h>
 int strlen(char a[])
 {
     int count;
-    for ( count = 0; count < a[count]; count++);
+    for (count = 0; count < a[count]; count++)
+        ;
     return count;
 }
 
 bool strcmp(char a[], char b[])
 {
-    if(strlen(a)!=strlen(b))
+    if (strlen(a) != strlen(b))
         return 0;
 
     int count = strlen(a);
     for (int i = 0; i < count; i++)
     {
-        if (a[i]!=b[i])
-            return 0;        
+        if (a[i] != b[i])
+            return 0;
     }
     return 1;
 }
 
 int main()
 {
-    int n;
-    while (scanf("%d",&n)!=EOF)
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int n, c = 0;
+    while (scanf("%d\n", &n) != EOF)
     {
-        char names[n][12];
+        char names[n][13];
         for (int i = 0; i < n; i++)
         {
-            scanf("%s",&names[i]);
+            scanf("%s", &names[i]);
         }
-        int worths[n], spends[n];
-        char tName[12];
+        int worths[n] = {0}, spends[n] = {0}, spend, toGift;
+        char name[13];
         for (int i = 0; i < n; i++)
         {
-            scanf("%s",&tName);
+            scanf("%s %d %d", &name, &spend, &toGift);
+            int index;
+            if (toGift == 0)
+            {
+                spend = 0;
+            }
+            else
+            {
+                spend -= spend % toGift;
+            }
             for (int j = 0; j < n; j++)
             {
-                if(strcmp(tName, names[i]))
-                    spends[i] = 
-            }            
-        }        
-    }    
+                if (strcmp(name, names[j]))
+                {
+                    index = j;
+                    break;
+                }
+            }
+            spends[index] += spend;
+            int gift = 0;
+            if (spend != 0 && toGift != 0)
+                gift = spend / toGift;
+            for (int j = 0; j < toGift; j++)
+            {
+                scanf("%s", &name);
+                for (int k = 0; k < n; k++)
+                {
+                    if (strcmp(name, names[k]))
+                    {
+                        worths[k] += gift;
+                        break;
+                    }
+                }
+            }
+        }
+        if (c > 0)
+            printf("\n");
+        for (int i = 0; i < n; i++)
+        {
+            printf("%s %d\n", names[i], worths[i] - spends[i]);
+        }
+        c++;
+    }
     return 0;
 }
